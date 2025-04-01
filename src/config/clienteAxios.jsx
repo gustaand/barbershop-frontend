8f2108b6e31afd6178f5376321916839a84b84e2
Gustavo Andrew <gusta.and21@gmail.com>
2025-04-01 22:49:08 +0200

    update backendURL

--- src/config/clienteAxios.jsx
+++ src/config/clienteAxios.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   // baseURL: `http://192.168.1.143:4000/api`,
+  baseURL: `https://barbershop-backend-jgst.onrender.com`,
   withCredentials: true,  // <--- Permite enviar las cookies directamente en la solicitud.
 })
 
