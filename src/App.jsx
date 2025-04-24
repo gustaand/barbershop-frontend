e23ece9ee52964c810e0d2f1508561c06cf37c17
Gustavo Andrew <gusta.and21@gmail.com>
2025-04-24 20:18:18 +0200

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
 import { AdminProvider } from './context/AdminProvider';
+import { ThemeProvider } from './context/ThemeProvider';
 import LoginAdmin from './pages/LoginAdmin';
 import AdminLayout from './layouts/AdminLayout';
 import Home from './pages/Home';
@@ -12,20 +13,22 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <AdminProvider>
-          <Routes>
-            {/* Redirige la raíz '/' a '/login-admin' */}
-            <Route path='/' element={<Navigate to="/login-admin" replace />} />
+          <ThemeProvider>
+            <Routes>
+              {/* Redirige la raíz '/' a '/login-admin' */}
+              <Route path='/' element={<Navigate to="/login-admin" replace />} />
 
-            <Route path='/login-admin' element={<LoginAdmin />} />
+              <Route path='/login-admin' element={<LoginAdmin />} />
 
-            <Route path='/admin' element={<AdminLayout />}>
-              <Route index element={<Home />} />
-              <Route path='citas' element={<Citas />} />
-              <Route path='citas/:id' element={<></>} />
-              <Route path='horarios' element={<Horarios />} />
-              <Route path='horarios/:id' element={<></>} />
-            </Route>
-          </Routes>
+              <Route path='/admin' element={<AdminLayout />}>
+                <Route index element={<Home />} />
+                <Route path='citas' element={<Citas />} />
+                <Route path='citas/:id' element={<></>} />
+                <Route path='horarios' element={<Horarios />} />
+                <Route path='horarios/:id' element={<></>} />
+              </Route>
+            </Routes>
+          </ThemeProvider>
         </AdminProvider>
       </AuthProvider>
     </BrowserRouter>
